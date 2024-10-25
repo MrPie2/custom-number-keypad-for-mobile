7d2db32d2b9308bd9a909b3af2aa5fcc42cee3df
Israel Damiekumo <israeldamiekumo@gmail.com>
2024-10-25 04:16:23 +0100

    Seperate the css and javascripts from the html

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,95 +1,56 @@
-var current=1;
-var next=current+1;
-$(document).keyup(function(event){
-    if(event.keyCode==8){
-        $('.pin').prev().focus()
-    }
-
-})
 
 var current=0;
-
-$('.pin').keyup(function(){
-    current++;
-    var next=current+1;
-    if(event.keyCode!=8){
-        if(next==5){
-            var password1=$('#pin1').val();
-        var password2=$('#pin2').val();
-        var password3=$('#pin3').val();
-        var password4=$('#pin4').val();
-
-        if(password1!=""&& password2!="" && password3!="" &&password4!=""){
-            setTimeout(function(){
-            $(".Confirm_Pin").removeClass('hidden');
-            $(".First_Try").addClass('hidden');
-            $('.spi').toggleClass('hidden');
-            }, 5000)
-        }else{
-            alert('Error');
-        }
-        $('.spi').html("<span class='fa fa-spinner fa-spin' style='font-size: 40pt; color: white'></span>");
-
-        }else if(next==9){
-            var password5=$('#pin5').val();
-            var password6=$('#pin6').val();
-            var password7=$('#pin7').val();
-            var password8=$('#pin8').val();
-        
-        if(password5!=""&& password6!="" && password7!="" &&password8!=""){
-                setTimeout(function(){
-                alert("You PIN has been set successfully")
-                location.reload();
-
-                 }, 5000)
-        }else{
-            alert('Error');
-        }
-  
-                $('.spi').toggleClass('hidden');
-                $('.spi').html("<span class='fa fa-spinner fa-spin' style='font-size: 40pt; color: white'></span>");
-
- 
-        }else{
-                 $('#pin'+next).prop('disabled',false).focus();
-
-        }
-    }else{
+for(var number=1; number<13; number++) {
+  if (number === 11) {
+    $('.keypads').append("<li class='singlekey' style='color: white' id='"+0+"'><div>0</div></li>");
+  } else if (number === 12) {
+    $('.keypads').append("<li class='singlekey' style='color: white' id='"+number+"'><div><i class='fas fa-backspace' style='font-size: 12pt'></i></div></li>");
+  } else if (number === 10) {
+    $('.keypads').append(" <li class='singlekey' style='color: white' id='" + number + "'><div><i class='fa fa-check' style='font-size: 14pt'></i></div></li>");
+  } else {
+    $('.keypads').append("<li class='singlekey' style='color: white' id='" + number + "'><div>" + number + "</div></li>");
+  }
+};
 
 
-    }
-})
+$(document).on('click', '.singlekey', function(){
 
-
-let numbers = [];
-
-// Generate array from 0 to 10 using a for loop
-for (let i = 0; i <= 10; i++) {
-  numbers.push(i);
-}
-
-// Sort to place 0 after 9
-numbers.sort(function(a, b) {
-  if (a === 0) return 1;   // Place 0 after other numbers
-  if (b === 0) return -1;  // Place other numbers before 0
-  return a - b;            // Sort the rest in ascending order
-});
-
-console.log(numbers); // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0]
-
-    for(var i=1; i<10; i++){
-       $('.keypads').append("<li class='singlekey' style='color: white' id='"+i+"'><span>"+i+"</span></li>");
-    }
-
-
-$(document).on('click', 'singlekey', function(){
 var number=$(this).attr('id');
+if(number==12){
+$('.pin').prev().focus()
+$('.pin').val("");
+}else if(number==10){
+var pinn1=$('#pin1').val();
+var pinn2=$('#pin2').val()
+var pinn3=$('#pin3').val();
+var pinn4=$('#pin4').val();
+var correct_pin=pinn1+pinn2+pinn3+pinn4;
+var database_pin="3434";
 
-if($('#pin1').val().trim()==""){
-alert('i am blank');
+if(correct_pin==database_pin){
+alert("Good, Transaction Successful");
 }else{
 
-
+alert("Incorrect Pin");
 }
 
+}else{
+current++;
+ var next=current+1;
+if($('#pin1').val().trim()==""){
+$('#pin1').val(number);
+$('#pin'+next).prop('disabled',false).focus();
+}else if($('#pin2').val().trim()==""){
+$('#pin2').val(number);
+$('#pin'+next).prop('disabled',false).focus();
+
+}else if($('#pin3').val().trim()==""){
+$('#pin3').val(number);
+$('#pin'+next).prop('disabled',false).focus();
+}else if($('#pin4').val().trim()==""){
+$('#pin4').val(number);
+$('#pin'+next).prop('disabled',false).focus();
+}
+}
 })
+
